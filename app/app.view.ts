fac8ed682139f29d90b2872b0f63301b863ae7a4
jin <jin@hyoo.ru>
2021-07-11 13:13:03 +0800

    No strict by default

--- app/app.view.ts
+++ app/app.view.ts
@@ -24,7 +24,7 @@ namespace $.$$ {
 			const query = this.query().trim()
 			if( !query ) return ''
 			
-			return `"${ query }" ${ this.query_forbidden() }`
+			return `${ query } ${ this.query_forbidden() }`
 		}
 		
 		@ $mol_mem
