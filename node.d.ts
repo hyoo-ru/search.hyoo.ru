28977ac7f505ef30a222b8d4bb45488517211769
nin-jin <nin-jin@users.noreply.github.com>
2022-10-05 07:23:03 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2428,6 +2428,7 @@ declare namespace $ {
         spellcheck(): boolean;
         length_max(): number;
         selection(val?: any): readonly number[];
+        bring(): void;
         Edit(): $mol_textarea_edit;
         row_numb(id: any): number;
         highlight(): string;
--- web.d.ts
+++ web.d.ts
@@ -2379,6 +2379,7 @@ declare namespace $ {
         spellcheck(): boolean;
         length_max(): number;
         selection(val?: any): readonly number[];
+        bring(): void;
         Edit(): $mol_textarea_edit;
         row_numb(id: any): number;
         highlight(): string;
