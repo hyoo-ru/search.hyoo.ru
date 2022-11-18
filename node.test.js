8d7b8cd03b30ffbb31cff82098dd7769ed2bb7f5
nin-jin <nin-jin@users.noreply.github.com>
2022-11-18 07:07:14 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -4130,7 +4130,8 @@ var $;
                 ...super.field(),
                 src: this.uri(),
                 alt: this.title(),
-                loading: this.loading()
+                loading: this.loading(),
+                decoding: this.decoding()
             };
         }
         minimal_width() {
@@ -4144,6 +4145,9 @@ var $;
         }
         loading() {
             return "eager";
+        }
+        decoding() {
+            return "async";
         }
     }
     $.$mol_image = $mol_image;
--- web.d.ts
+++ web.d.ts
@@ -1140,11 +1140,13 @@ declare namespace $ {
             src: string;
             alt: string;
             loading: string;
+            decoding: string;
         };
         minimal_width(): number;
         minimal_height(): number;
         uri(): string;
         loading(): string;
+        decoding(): string;
     }
 }
 
