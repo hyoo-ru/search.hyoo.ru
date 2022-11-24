365ed6f69d588296b021645ca2b2a098e57671f7
nin-jin <nin-jin@users.noreply.github.com>
2022-11-24 07:06:48 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -9478,7 +9478,8 @@ var $;
             if (key === 'id')
                 continue;
             if (typeof props[key] === 'string') {
-                ;
+                if (key in node)
+                    node[key] = props[key];
                 node.setAttribute(key, props[key]);
             }
             else if (props[key] &&
