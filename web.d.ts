a6c7a17742d4f8ccbe96e5ac285340a06953aeae
nin-jin <nin-jin@users.noreply.github.com>
2026-01-24 07:17:59 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -2701,6 +2701,7 @@ declare namespace $ {
 		bubble_content( ): readonly(any)[]
 		Filter( ): $mol_search
 		Trigger_icon( ): $mol_icon_dots_vertical
+		trigger_enabled( ): ReturnType< $mol_select['enabled'] >
 	}
 	
 }
