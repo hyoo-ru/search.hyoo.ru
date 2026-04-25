6415e8a5fb40e8528c7d80294991c632bafbc567
nin-jin <nin-jin@users.noreply.github.com>
2026-04-25 08:03:32 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -2423,6 +2423,8 @@ var $;
         'field',
         'image',
         'spirit',
+        'hue',
+        'hue_spread',
     ]);
 })($ || ($ = {}));
 
@@ -3945,7 +3947,7 @@ var $;
                         }
                     }
                 }
-                else if (key === '@media') {
+                else if (key === '@media' || key === '@container') {
                     const media = config[key];
                     for (let query of Object.keys(media).reverse()) {
                         rules.push('}\n');
--- web.mjs
+++ web.mjs
@@ -296,6 +296,8 @@ var $;
         'field',
         'image',
         'spirit',
+        'hue',
+        'hue_spread',
     ]);
 })($ || ($ = {}));
 
@@ -3385,7 +3387,7 @@ var $;
                         }
                     }
                 }
-                else if (key === '@media') {
+                else if (key === '@media' || key === '@container') {
                     const media = config[key];
                     for (let query of Object.keys(media).reverse()) {
                         rules.push('}\n');
