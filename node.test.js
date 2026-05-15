430679ed4f1db22babc6bd864a002a03c286c805
nin-jin <nin-jin@users.noreply.github.com>
2026-05-15 09:44:51 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -785,6 +785,12 @@ var $;
         // [ Symbol.toPrimitive ]() {
         // 	return this.toString()
         // }
+        match(...args) {
+            return this.toString().match(...args);
+        }
+        split(...args) {
+            return this.toString().split(...args);
+        }
         toString() {
             return this.join('\n');
         }
--- web.js
+++ web.js
@@ -1097,6 +1097,12 @@ var $;
         // [ Symbol.toPrimitive ]() {
         // 	return this.toString()
         // }
+        match(...args) {
+            return this.toString().match(...args);
+        }
+        split(...args) {
+            return this.toString().split(...args);
+        }
         toString() {
             return this.join('\n');
         }
